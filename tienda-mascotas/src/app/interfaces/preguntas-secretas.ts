e2fe48596a406305781e18771916c602468f25e4
Roberto Isla <robertoisla92@gmail.com>
2021-06-15 10:28:43 -0400

    Creación interfaz que exporta listado de preguntas secretas para la creación de usuario

---
+++ tienda-mascotas/src/app/interfaces/preguntas-secretas.ts
@@ -0,0 +1,30 @@
+export interface PreguntasSecretas {
+    pregunta : string;
+}
+
+export const ListadoPreguntas : Array<PreguntasSecretas> = [
+    {
+        pregunta:"¿Cual es el nombre de su banda favorita?"
+    },
+    {
+        pregunta:"¿Cómo se llamó su segunda mascota?"
+    },
+    {
+        pregunta:"¿En donde estudio y completó su educación media?"
+    },
+    {
+        pregunta:"¿Cuál es su juego favorito?"
+    },
+    {
+        pregunta:"¿Donde fue su primer romance?"
+    },
+    {
+        pregunta:"¿Cómo se llama su libro favorito?"
+    },
+    {
+        pregunta:"¿Cuál es su marca preferida de ropa?"
+    },
+    {
+        pregunta:"¿Donde realizó su primer encuentro deportivo?"
+    }
+]
