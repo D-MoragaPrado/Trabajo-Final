b5dfa91c4cf749de022e1419b0eb6ee941d6f17f
D-MoragaPrado <diana.moragaprado@gmail.com>
2021-06-23 19:38:04 -0400

    .intento filtros
    siguen fallando

--- tienda-mascotas/src/app/components/categorias/categorias.component.ts
+++ tienda-mascotas/src/app/components/categorias/categorias.component.ts
@@ -17,12 +17,13 @@ export class CategoriasComponent implements OnInit {
   formFiltro:FormGroup;
   constructor(private servicioProducto : ManejoProductosService,private rutaActiva: ActivatedRoute,private fb:FormBuilder) {
     this.formFiltro=this.fb.group({
+      Disponible:[false],
+      NoDisponible:[false],
       min:[null],
       max:[null],
       Rating1:[0],
       Rating2:[0],
-      Disponible:[false],
-      NoDisponible:[false],
+      
     })
    }
   
@@ -31,9 +32,9 @@ export class CategoriasComponent implements OnInit {
       this.servicioProducto.getProductosPerro().subscribe((producto) => {
         for (let i = 0; i < producto.length; i++) {
           this.productos.push(producto[i]);
+          this.productosfiltro.push(producto[i])
           console.log(producto[i]);
         }
-        this.productosfiltro=this.productos;
       });
     }else if(this.rutaActiva.snapshot.params.animal=="Gato"){
       this.servicioProducto.getProductosGato().subscribe((producto) => {
@@ -48,9 +49,14 @@ export class CategoriasComponent implements OnInit {
 
   Filtrar(){
     this.productos=[];
+    console.log("esto deberia estar vacio",this.productos,this.productos.length)
     //Disponibilidad
     let disponible=this.formFiltro.get("Disponible")?.value;
     let nodisponible=this.formFiltro.get("NoDisponible")?.value;
+    let pmin=this.formFiltro.get("min")?.value;
+    let pmax=this.formFiltro.get("max")?.value;
+
+    console.log(pmin,pmax,pmin+pmax)
     if(disponible && !nodisponible){
       for (let i = 0; i < this.productosfiltro.length; i++) {
         if(this.productosfiltro[i].stock>0){
@@ -60,7 +66,10 @@ export class CategoriasComponent implements OnInit {
     }else if((disponible && nodisponible)||(!disponible && !nodisponible)){
       for (let i = 0; i < this.productosfiltro.length; i++) {
         this.productos.push(this.productosfiltro[i]);
+        console.log("metiendoo",this.productosfiltro[i]);
+        console.log("asi va quedando: ",this.productos);
       }
+      console.log("entre a la primera cosa",this.productos,this.productosfiltro);
 
     }else if(nodisponible && !disponible){
       for (let i = 0; i < this.productosfiltro.length; i++) {
@@ -69,29 +78,35 @@ export class CategoriasComponent implements OnInit {
         }
       }
     }
-    console.log("primero todos los productos son: ",this.productosfiltro);
+    
+    console.log("primero los productos son: ",this.productos);
+
+
+    //aqui todo se derrumbó
     //rango de precios
-    let pmin=this.formFiltro.get("min")?.value;
-    let pmax=this.formFiltro.get("max")?.value;
+    
     if((pmin!=null) && (pmax!=null) &&(pmin>=0)&&(pmax>=0)){
       if(pmin<=pmax){
+        console.log("al entrar a los precios: ",this.productos);
         for (let i = 0; i < this.productos.length; i++) {
+          console.log(this.productos[i]);
           if((this.productos[i].precio<pmin)||(this.productos[i].precio>pmax)){
             this.productos.splice(i,1);
+          }else{
+            console.log("productos excluidos precio",this.productos[i].precio+1);
           }
         }
       }else{
         alert("Rango de precios NO válido")
       }
     }
-
-    console.log("segundo todos los productos son: ",this.productosfiltro);
+    console.log("segundo los productos son: ",this.productos);
+    
     //rango de valoracion
     
     let rmin=parseInt(this.formFiltro.get("Rating1")?.value);
     let rmax=parseInt(this.formFiltro.get("Rating2")?.value);
     if(rmax!=0 ){
-      console.log(this.productos,this.productos.length);
       console.log(" entre ",rmin," y ",rmax)
       if(rmin<=rmax){
         for (let j = 0; j < this.productos.length; j++) {
@@ -106,6 +121,7 @@ export class CategoriasComponent implements OnInit {
         alert("Rango de valoración NO válido")
       }
     }
+    console.log("tercero los productos son: ",this.productos);
 
 
   }
